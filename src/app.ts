eabb26567dd1c8c5ca47dfa31a8b31ed79b6bc39
Horob1 <nguyentheanh24102003@gmail.com>
2024-09-10 14:56:52 +0700

    add serve fe and data

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cron from 'node-cron'
 import { sendLogAtEndOfDays } from './utils/cron'
 import router from './routes'
 import cors from 'cors'
+import path from 'path'
 const app = express()
 
 const httpServer = createServer(app)
@@ -13,15 +14,22 @@ app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!')
+// })
 app.use('/api', router)
 // cron.schedule('* * * * *', sendLogAtEndOfDays)
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.log('Error:', err.message)
   res.status(404).json({ error: err.message })
 })
+
+app.use('/', express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
 cron.schedule('5 0 * * *', sendLogAtEndOfDays)
 
 initSocket(httpServer)
